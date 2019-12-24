9a42715d1a9e06af8bb1501ad231f3e7e7cc373a
knielbo <kln@cas.au.dk>
2019-12-24 10:08:00 +0100

    adds read att for tabular

--- src/semantic_vect.py
+++ src/semantic_vect.py
@@ -1,32 +1,51 @@
 #!/home/knielbo/virtenvs/nuke/bin/python
 
 import os
+import pandas as pd
 
 
 class Corpus(object):
     """ A basic corpus class for reading documents ('content') from vanilla or tabular files
     
     Attributes:
-        name: A string representing the corpus' name
-        path: A string representing the relative path
+        name: str representing the corpus' name
+        path: str representing the relative path
     """
 
     def __init__(self, name, path):
         self.name = name
         self.path = path
     
-    def read_files(self):
+    def read(self):
+        """ Reads filenames and content of file(s) on path
+
+        Attributes:
+            fnames: list of str containing filename on files on path 
+            fpaths: list of str containing relative path and filename to files on path
+            content
+        """
         self.fnames = os.listdir(self.path)
+        self.fpaths = [os.path.join(self.path,fname) for fname in self.fnames]
+        if len(self.fnames) > 1:
+            self.content = list()
+            for fname in self.fpaths:
+                with open(fname, "r") as f:
+                    self.content.append(f.read())
+        else:
+            df = pd.read_csv(self.fpaths[0])
+            self.content = df.content.values
+
+    
 
 
 
 
 def main():
-    print("\n test main() for semantic_vect \n") 
+    print("\n\n test main() for semantic_vect \n\n") 
     fpath = os.path.join("dat","tabular")
     DATA = Corpus("foobar", fpath)
-    DATA.get_fnames()
-    print(DATA.fnames)
+    DATA.read()
+    print(DATA.content[0])
 
 if __name__ == "__main__":
     main()
