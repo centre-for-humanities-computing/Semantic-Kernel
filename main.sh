15c3048edae8c361a16503fd8398721e76d8d3ce
knielbo <kln@cas.au.dk>
2020-01-22 14:50:22 +0100

    add sec timing

--- main.sh
+++ main.sh
@@ -2,10 +2,11 @@
 
 # pipeline for Nucleus
 ## suppress warnings on call
+start=`date +%s`
 echo pipeline init
-while true;do echo -n ':( ';sleep 1;done &
+while true;do echo -n '>';sleep 1;done &
 
-python src/semantic_vect.py
+python src/semantic_vect.py #> log_corpus.txt
 #python build_nucleus.py
 #python -W ignore build_graph.py
 
@@ -13,3 +14,7 @@ python src/semantic_vect.py
 kill $!; trap 'kill $!' SIGTERM
 echo
 echo ':)'
+
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
